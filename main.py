433de4c197185ca5b8ef4706ddc71a1efb703446
harshit joshi <harshitjoshi3636@gmail.com>
2026-07-24 13:00:52 +0530

    registered Reversal Red

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from jujutsu_engine import JujutsuEngine, InfiniteVoid
+from jujutsu_engine import JujutsuEngine, InfiniteVoid , ReversalRed
 
 def main():
     engine = JujutsuEngine(camera_index=0)
     
     void_expansion = InfiniteVoid("D:/interactive-media-engine/techniques/unlimited_void.mp4")
+    reversal_red =  ReversalRed("D:/interactive-media-engine/techniques/reversal_red.mp4")
     
     engine.register_technique(void_expansion)
+    engine.register_technique(reversal_red)
     
     engine.start()
 
